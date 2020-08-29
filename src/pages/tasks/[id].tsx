fbfc862b326644aef9c46e773c1ab3f54eacca29
みずしま <mizuki.bz.3224@gmail.com>
2020-08-29 10:30:57 +0900

    create task  page

---
+++ src/pages/tasks/[id].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+const Task = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    return(
+        <>
+            <h2>This is Task Page</h2>
+            <p>param:{id}</p>
+        </>
+    )
+}
+export default Task
